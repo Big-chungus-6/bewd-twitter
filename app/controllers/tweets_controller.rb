49830ed462a61bf2e02c7ce5d26ae3352453ae0a
h8rry <harrychenca@gmail.com>
2023-07-28 22:43:18 +0200

    Pass more tests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,40 +1,40 @@
 class TweetsController < ApplicationController
-    def create
-        @tweet = current_user.tweets.new(tweet_params)
-      
-        if @tweet.save
-          render json: @tweet, status: :created
-        else
-          render json: @tweet.errors, status: :unprocessable_entity
-        end
-      end
-      
-      def destroy
-        @tweet = current_user.tweets.find(params[:id])
-        @tweet.destroy
-        head :no_content
-      end
-      
-      def index
-        @tweets = Tweet.all
-        render json: @tweets
-      end
-      
-      def index_by_user
-        @user = User.find_by(username: params[:username])
-        @tweets = @user.tweets
-        render json: @tweets
-      end
-      
-      private
-      def tweet_params
-        params.require(:tweet).permit(:message)
-      end
-      
-      def current_user
-        token = cookies[:twitter_session_token]
-        session = Session.find_by(token: token)
-        User.find(session.user_id)
-      end
-      
+  def create
+    @tweet = current_user.tweets.new(tweet_params)
+
+    if @tweet.save
+      render json: @tweet, status: :created
+    else
+      render json: @tweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tweet = current_user.tweets.find(params[:id])
+    @tweet.destroy
+    head :no_content
+  end
+
+  def index
+    @tweets = Tweet.all
+    render json: @tweets
+  end
+
+  def index_by_user
+    @user = User.find_by(username: params[:username])
+    @tweets = @user.tweets
+    render json: @tweets
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message)
+  end
+
+  def current_user
+    token = cookies[:twitter_session_token]
+    session = Session.find_by(token: token)
+    User.find(session.user_id)
+  end
 end
